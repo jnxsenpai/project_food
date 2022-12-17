8f0dfb50a6913604d4acfb41c4b1575b75004e1f
Maksim <jnxsenpai@yandex.ru>
2022-12-17 16:30:45 +0300

    v2

--- js/script.js
+++ js/script.js
@@ -197,38 +197,24 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
     
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-        'menu__item'  // передача нового класса. С использованием rest все последующие аргументы также будут передаваться в один массив
+    const getResource = async (url) => { 
+        const res = await fetch(url);
 
-    ).render();
+        if (!res.ok) {  // Если возникает ошибка в запросе, функция возвратит ошибку со статусом
+           throw new Error(`Could not feth ${url}, status: ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        9,
-        '.menu .container',
-        'menu__item'     // без точки, тк используем его в classList
+        return await res.json();       // получает промис и возвращает его в json
+    };
 
-    ).render();
+    getResource('http://localhost:3000/menu') // при помощи запроса мы получаем массив в сменю из базы
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => { // перебираем массив и тк внутри у нас массив, диструктуризируем его по отдельным свойствам
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();  // создаем новые объекты класса MenuCard() с переданными свойствами из объекта c меню и рендерим
+            });
+        });
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        9,
-        '.menu .container',
-        'menu__item' 
-
-    ).render();
+    
     
     //FORMS
 
@@ -241,10 +227,22 @@ window.addEventListener("DOMContentLoaded", () => {
     };
 
     forms.forEach(item => { // перебираем все наши формы, тк они идут в массиве item - это наша форма 
-        postData(item);     // вызываем функцию postData с каждой из форм по очереди
+        bindPostData(item);     // вызываем функцию postData с каждой из форм по очереди
     });
     
-    function postData(form) {
+    const postData = async (url, data) => { // функция отправляет  запрос на сервер и получает данные
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                    'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();       // получает промис и возвращает его в json
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();                // отменяем стандартную перезагрузку страницы
 
@@ -260,21 +258,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
             const formData = new FormData(form); //переводим данные в формат formData. В скобках форма из которой забираем данные
+            console.log(formData);
 
-            const object = {};
-            formData.forEach(function(value, key){   // тут мы перебираем данные из формата форм даты и переносим их в обычный объект
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries())); // переводим данные FormData в json
+            //сначала переводим данные FormData в масив масивов с помощью formData.entries(), а после в обычный объект с помощью Object.fromEntries
 
 
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object) // далее, преобразуем в формат json
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', JSON.stringify(json))
             .then(data => {
                 console.log(data);
                     showThanksModal(message.success); // если отправка данных успешно завершилась, то отправляется сообщение success
@@ -315,6 +305,10 @@ window.addEventListener("DOMContentLoaded", () => {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 });
 
 
